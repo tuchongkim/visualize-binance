67965d764f4d6039c58f9af32effb89871c10187
Tuchong Kim <2019102076@khu.ac.kr>
2023-11-17 19:02:22 +0900

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, jsonify
+import config
+import time
+from datetime import datetime
+from binance.client import Client
+
+app = Flask(__name__)
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+current_time = time.time()
+
+pnl_and_time = []
+
+@app.route('/')
+def index():
+    title = 'BinanceView'
+
+    info = client.futures_account_balance()
+
+    return render_template('index.html', title=title, my_balances=info)
+
+@app.route('/buy')
+def buy():
+    return 'buy'
+
+@app.route('/sell')
+def sell():
+    return 'sell'
+
+@app.route('/settings')
+def settings():
+    return 'settings'
+
+@app.route('/history')
+def history():
+    candlesticks = client.futures_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "10 Nov, 2023")
+
+    processed_candlesticks = []
+
+    # { time: '2018-10-19', open: 180.34, high: 180.99, low: 178.57, close: 179.85 }
+    for data in candlesticks:
+        candlestick = {
+            "time": data[0] / 1000, 
+            "open": data[1], 
+            "high": data[2], 
+            "low": data[3], 
+            "close": data[4]
+        }
+
+        processed_candlesticks.append(candlestick)
+
+    return jsonify(processed_candlesticks)
+
+# @app.route('/pnl')
+# def pnl():
+#     accountInfo = client.futures_account()
+
+#     # { time: "2018-03-28", value: 154 },
+
+#     for balance in accountInfo['assets']:
+#         if balance['asset'] == 'USDT':
+#             pnl = float(balance['crossUnPnl'])
+#             data = {
+#                 "time" : datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+#                 "value" : pnl
+#             }
+#             pnl_and_time.append(data)
+
+#     return jsonify(pnl_and_time)
+
+
+# @app.route()
+# def
